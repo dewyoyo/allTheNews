74ee4cd828db9412b24999c6b3dbaf1b9b7efbee
Christine Lee <dewyoyo@hotmail.com>
2023-03-16 06:10:02 -0600

    Update_callback

--- controllers/index.js
+++ controllers/index.js
@@ -31,7 +31,7 @@ router.get('/', function (req, res) {
 //                        articles: articles
 //                    };
         .then((articles) => {
-            console.log(articles);
+            //console.log("getarticles:" + articles);
             let hbsObj = {
                 title: 'All the News That\'s Fit to Scrape',
                 subtitle: 'NPR News',
@@ -126,95 +126,106 @@ router.get('/api/articles/saved', function (req, res) {
 
 // save an article
 router.post('/api/articles/save/:id', function (req, res) {
-    Article.findByIdAndUpdate(req.params.id, {
-        $set: { saved: true }
-    },
-        { new: true },
-        function (error, doc) {
-            if (error) {
-                console.log(error);
-                res.status(500);
-            } else {
-                res.redirect('/');
-            }
-        });
+ //   Article.findByIdAndUpdate(req.params.id, {
+ //       $set: { saved: true }
+//    },
+//        { new: true },
+//        function (error, doc) {
+//            if (error) {
+//                console.log(error);
+//                res.status(500);
+//            } else {
+//                res.redirect('/');
+//            }
+//        });
+
+    Article.findByIdAndUpdate(req.params.id, { $set: { saved: true } }, { new: true }).then((docs) => {
+        if (docs) {
+            console.log("success");
+            res.redirect('/saved');
+        } else {
+            console.log("no such article exist");
+        }
+    }).catch((err) => {
+        console.log(err);
+    })
+
+
 });
 
 
 // delete a saved article
 router.delete('/api/articles/:id', function (req, res) {
-    Article.findByIdAndUpdate(req.params.id,
-        { $set: { deleted: true } },
-        { new: true },
-        function (error, doc) {
-            if (error) {
-                console.log(error);
-                res.status(500);
-            } else {
-                res.redirect('/saved');
-            }
-        }
-    );
+    Article.findByIdAndUpdate(req.params.id, { $set: { deleted: true } }, { new: true }).then((error) => {
+            res.redirect('/saved');
+    }).catch((err) => {
+        console.log(err);
+        res.status(500);
+    })
+
 });
 
 // restore a saved article
 router.post('/api/restore/:id', function (req, res) {
-    Article.findByIdAndUpdate(req.params.id,
-        { $set: { deleted: false } },
-        { new: true },
-        function (error, doc) {
-            if (error) {
-                console.log(error);
-                res.status(500);
-            } else {
-                res.redirect('/deleted');
-            }
-        }
-    );
+    Article.findByIdAndUpdate(req.params.id, { $set: { deleted: false } }, { new: true }).then((error) => {
+            res.redirect('/deleted');
+    }).catch((err) => {
+        consosle.log(err);
+        res.status(500);
+    })
+
 });
 
 // scrape articles
 router.get('/api/articles/scrape', function (req, res, next) {
     // First, we grab the body of the html with axios
-    axios.get("https://www.npr.org/sections/news/").then(function (response) {
+    axios.get("https://www.npr.org/sections/news/").then((response) => {
         // Then, we load that into cheerio and save it to $ for a shorthand selector
-        var $ = cheerio.load(response.data);
 
-        console.log($("article"));
+        //console.log(response);
+        var $ = cheerio.load(response.data);
+        //console.log("response.data:" + response.data);
+
+        //console.log($("article"));
+
+        //let article = $('article');
+        //console.log(article.text());
 
         // Now, we grab every h2 within an article tag, and do the following:
-        $("article").each(function (i, element) {
+        $("article").each((i, element) => {
             // Save an empty result object
             var result = {};
             //$("article").children('.item-image').children('.imagewrap').children('a').children('img').children('src');
 
-            if ($(this).children('.item-image').children('.imagewrap').children('a').children('picture').children('img').attr('src') == null) {
+            if ($(element).children('.item-image').children('.imagewrap').children('a').children('picture').children('img').attr('src') == null) {
                 next();
             }
             else {
                 // Add the text and href of every link, and save them as properties of the result object
-                result.title = $(this).children('.item-info-wrap').children('.item-info').children('.title').children('a').text();
-                result.link = $(this).children('.item-info-wrap').children('.item-info').children('.teaser').children('a').attr('href');
-                result.summary = $(this).children('.item-info-wrap').children('.item-info').children('.teaser').children('a').text();
-                result.imageURL = $(this).children('.item-image').children('.imagewrap').children('a').children('picture').children('img').attr('src');
+                result.title = $(element).children('.item-info-wrap').children('.item-info').children('.title').children('a').text();
+                result.link = $(element).children('.item-info-wrap').children('.item-info').children('.teaser').children('a').attr('href');
+                result.summary = $(element).children('.item-info-wrap').children('.item-info').children('.teaser').children('a').text();
+                result.imageURL = $(element).children('.item-image').children('.imagewrap').children('a').children('picture').children('img').attr('src');
 
-
+                //console.log(result.imageURL);
                 // create new article
                 let entry = new Article(result);
+                //console.log(entry);
                 // save to database
-                entry.save((doc) => {
+                entry.save().then((doc) => {
                     console.log('new article added');
                 }).catch((err) => {
-                    console.log(err);
+                    console.log(err.message);
                 });
-
 
             }
 
-
-
+        }).catch((err) => {
+            console.log(err);
         });
         next();
+    }).catch((err) => {
+        console.log(err);
     });
 
 
@@ -243,36 +254,64 @@ router.get('/api/notes', function (req, res) {
 router.post('/api/notes/:id', function (req, res) {
     let newNote = new Note(req.body);
     console.log(req.body);
-    newNote.save(function (err, doc) {
-        if (err) {
+//    newNote.save(function (err, doc) {
+//        if (err) {
+//            console.log(err);
+//            res.status(500);
+//        } else {
+//            Article.findOneAndUpdate(
+//                { _id: req.params.id },
+//                { $push: { 'notes': doc.id } },
+//                function (error, newDoc) {
+//                    if (error) {
+//                        console.log(error);
+//                        res.status(500);
+//                    } else {
+//                        res.redirect('/saved');
+//                    }
+//                }
+//            );
+//        }
+//    });
+
+    // save to database
+    newNote.save().then((doc) => {
+        console.log('new note added');
+
+        //if the new note created, then update the article
+        Article.findByIdAndUpdate(req.params.id, { $push: { 'notes': doc.id } }).then((docs) => {
+            if (docs) {
+                console.log("success");
+                res.redirect('/saved');
+            } else {
+                console.log("no such article exist");
+            }
+        }).catch((err) => {
             console.log(err);
-            res.status(500);
-        } else {
-            Article.findOneAndUpdate(
-                { _id: req.params.id },
-                { $push: { 'notes': doc.id } },
-                function (error, newDoc) {
-                    if (error) {
-                        console.log(error);
-                        res.status(500);
-                    } else {
-                        res.redirect('/saved');
-                    }
-                }
-            );
-        }
+        });
+
+    }).catch((err) => {
+        console.log(err.message);
     });
+
+
+
 });
 
 // delete a note from a saved article
 router.delete('/api/notes/:id', function (req, res) {
-    Note.findByIdAndRemove(req.params.id, function (err, note) {
-        if (err) {
-            console.log(err);
-            res.status(500);
-        } else {
+    Note.findByIdAndRemove(req.params.id).then((note) => {
+
+        if (note) {
+            console.log("success");
             res.redirect('/saved');
+        } else {
+            console.log("no such article exist");
         }
+
+    }).catch((err) => {
+        console.log(err);
+        res.status(500);
     });
 });
 
